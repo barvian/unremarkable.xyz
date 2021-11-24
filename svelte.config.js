3eb56d6d25e1c70a4b97f4b74d23b4f33e84bfcc
Maxwell Barvian <max@barvian.me>
2021-11-24 05:30:30 -0600

    Add Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
+import vercel from '@sveltejs/adapter-vercel'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		target: '#svelte',
+		adapter: vercel(),
 		vite: {
             define: {
                 'process.env': process.env // for Vercel only, Vite doesn't expose these by default
